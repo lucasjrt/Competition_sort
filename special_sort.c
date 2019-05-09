fa372fb1f94c56e08ea7f6c513ff692c3ae221b4
Lucasjrt <lucasjrt@outlook.com>
2019-05-09 14:02:52 -0300

    Esse talvez funcione

--- special_sort.c
+++ special_sort.c
@@ -5,7 +5,7 @@
 #define ROWS_SIZE 100000
 
 char **caux;
-int *cint;
+int *iaux;
 
 void stringMerge(char **v, int inicio, int fim) {
     int v1 = inicio, meio = inicio + ((fim - inicio) >> 1), v2 = meio + 1, tamanho = fim - inicio + 1, flag1 = 0, flag2 = 0;
@@ -13,20 +13,20 @@ void stringMerge(char **v, int inicio, int fim) {
     for(i = 0; i < tamanho; i++) {
         if(!flag1 && !flag2) {
             if(strcmp(v[v1], v[v2]) < 0) 
-                strcpy(aux[i], v[v1++]);
+                strcpy(caux[i], v[v1++]);
             else
-                strcpy(aux[i], v[v2++]);
+                strcpy(caux[i], v[v2++]);
         } else {
             if(!flag1)
-                strcpy(aux[i], v[v1++]);
+                strcpy(caux[i], v[v1++]);
             else 
-                strcpy(aux[i], v[v2++]);
+                strcpy(caux[i], v[v2++]);
         }
         if(v1 > meio) flag1 = 1;
         if(v2 > fim)  flag2 = 1;
     }
     for(i = 0, j = inicio; j < fim + 1; i++, j++) {
-        strcpy(v[j], aux[i]);
+        strcpy(v[j], caux[i]);
     }
 }
 
@@ -40,30 +40,30 @@ void _mergeString(char **v, int inicio, int fim) {
 
 }
 
-inline void mergeString(char **v, int n) {
+void mergeString(char **v, int n) {
     _mergeString(v, 0, n-1);
 }
 
-void lenMerge(char **v, int inicio, int fim) {
+void mergeLen(char **v, int inicio, int fim) {
     int v1 = inicio, meio = inicio + ((fim - inicio) >> 1), v2 = meio + 1, tamanho = fim - inicio + 1, flag1 = 0, flag2 = 0;
     int i, j;
     for(i = 0; i < tamanho; i++) {
         if(!flag1 && !flag2) {
             if(strlen(v[v1]) < strlen(v[v2]))
-                strcpy(aux[i], v[v1++]);
+                strcpy(caux[i], v[v1++]);
             else
-                strcpy(aux[i], v[v2++]);
+                strcpy(caux[i], v[v2++]);
         } else {
             if(!flag1)
-                strcpy(aux[i], v[v1++]);
+                strcpy(caux[i], v[v1++]);
             else 
-                strcpy(aux[i], v[v2++]);
+                strcpy(caux[i], v[v2++]);
         }
         if(v1 > meio) flag1 = 1;
         if(v2 > fim)  flag2 = 1;
     }
     for(i = 0, j = inicio; j < fim + 1; i++, j++) {
-        strcpy(v[j], aux[i]);
+        strcpy(v[j], caux[i]);
     }
 }
 
@@ -72,15 +72,15 @@ void _lenMerge(char **v, int inicio, int fim) {
         int meio = inicio + ((fim - inicio) >> 1);
         _lenMerge(v, inicio, meio);
         _lenMerge(v, meio + 1, fim);
-        lenMerge(v, inicio, fim);
+        mergeLen(v, inicio, fim);
     }
 }
 
-inline void lenMerge(char **v, int n) {
+void lenMerge(char **v, int n) {
     _lenMerge(v, 0, n);
 }
 
-inline int ehBin(char *v) {
+int ehBin(char *v) {
     int i;
 
     for(i = 0; i < strlen(v); i++)
@@ -96,13 +96,13 @@ int main() {
     cinput[n] = (char*) malloc(COLS_SIZE * sizeof(char));
     i = scanf("%s", cinput[n]);
     if(i != EOF) { //TODO: Alterar esse if para dentro dos whiles
-        if(cinput[n] < '0' || cinput[n] > '9') {
+        if(cinput[n][0] < '0' || cinput[n][0] > '9') {
             caux = (char**) malloc(ROWS_SIZE * sizeof(char*));
             caux[n++] = (char*) malloc(COLS_SIZE * sizeof(char));
             cinput[n] = (char*) malloc(COLS_SIZE * sizeof(char));
             while(scanf("%s", cinput[n]) != EOF) {
                 cinput[n] = (char*) malloc(COLS_SIZE * sizeof(char));
-                aux[n] = (char*) malloc(COLS_SIZE * sizeof(char));
+                caux[n] = (char*) malloc(COLS_SIZE * sizeof(char));
                 n++;
             }
             mergeString(cinput, n);
@@ -112,9 +112,9 @@ int main() {
             cinput[n] = (char*) malloc(COLS_SIZE * sizeof(char));
             while(scanf("%s", cinput[n]) != EOF) {
                 cinput[n] = (char*) malloc(COLS_SIZE * sizeof(char));
-                aux[n] = (char*) malloc(COLS_SIZE * sizeof(char));
+                caux[n] = (char*) malloc(COLS_SIZE * sizeof(char));
             }
-            mergeBin(cinput, n);
+            //mergeBin(cinput, n);
         } else {
             iinput = (int*) malloc(ROWS_SIZE * sizeof(int));
             iinput[n] = atoi(cinput[n]);
@@ -129,7 +129,10 @@ int main() {
     for(i = 0; i <= n; i++) {
        printf("%s\n", cinput[i]); 
     }
-    free(aux);
+    if(caux != NULL)
+    	free(caux);
+	else
+		free(iaux);
     free(cinput);
     return 0;
 }
